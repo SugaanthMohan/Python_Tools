f15ed4d9fa58288301f4dea03a27cfa4e62aae46
Suganth-DMac <sugaanth.mohan@gmail.com>
2019-05-07 18:13:13 +0530

    Automation Error Handlers
    ID: Suganth-Mohan-201905-0718-1158
    Description: It is used for Signal handling and Errors handling which accompy both Internally and Externally.
    Language: Python
    Comments: -
    Co-Authored-By: KasiRamaKrishnan <kasiramakrishnan@users.noreply.github.com>

---
+++ SignalHandling/SignalHandling.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python3.5
+
+from time import sleep
+import sys
+import linecache
+
+
+
+
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>> MYDIE MODULE USED HERE <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+def mydie(exitCont_):
+
+    print(exitCont_)
+
+    print("*** ERROR OCCURRED *** : ROLL BACK PROCEDURES EXECUTING BELOW")
+
+    # DEFINE DB DISCONNECT HERE
+
+    print("*** ROLL BACK *** : CLOSING DB CONNECTION")
+
+    # DEFINE CLOSE FTP CONNECTION HERE
+
+    # DEFINE CLEARING THE TEMPORARY FILES HERE
+
+    # DEFINE CREATING A PICKLE HERE
+
+    sys.exit(0)
+
+
+# >>>>>>>>>>>>>>>>>>>>>>> EXCEPTION BRIEFER USED HERE <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+def ExceptionBrief():
+
+    # CREATE EXCEPTION REPORT
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    return 'EXCEPTION CAPTURED : ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
+
+
+def main():
+
+	sleep(10)
+
+
+if __name__=="__main__":
+
+    try:
+        main()
+    # check the type of the exception
+    # use hash attribute to print of hash type values
+    # and if it is a non hash type attribute,
+    # you can print the exception normally
+    except KeyboardInterrupt:
+        full_execption_report = ExceptionBrief()
+        full_execption_report = full_execption_report+" Program Interrupted By External Source"
+        mydie(full_execption_report)
+
